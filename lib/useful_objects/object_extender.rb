2547b44a52ff6e430c2e397fef6cb7c1d5455c73
k-motoyan <k.motoyan888@gmail.com>
2016-03-19 13:21:29 +0900

    Implemented to_bool method.

--- lib/useful_objects/object_extender.rb
+++ lib/useful_objects/object_extender.rb
@@ -3,6 +3,23 @@ module UsefulObjects
   module ObjectExtender
     refine Object do
       def to_bool
+        case self
+        when TrueClass, FalseClass then self
+        when NilClass then false
+        when Integer, Float then convert_number_to_bool(self)
+        when String then convert_string_to_bool(self)
+        else true
+        end
+      end
+
+      private
+
+      def convert_string_to_bool(value)
+        value == 'false' ? false : true
+      end
+
+      def convert_number_to_bool(value)
+        value == 0 ? false : true
       end
     end
   end
