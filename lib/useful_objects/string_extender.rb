e4db44aafadca78c00e568e09a1f51e9d85a580b
k-motoyan <k.motoyan888@gmail.com>
2016-03-21 10:54:05 +0900

    Added string extender skelton.

---
+++ lib/useful_objects/string_extender.rb
@@ -0,0 +1,9 @@
+module UsefulObjects
+  # External String methods.
+  module StringExtender
+    refine String do
+      def parse_json
+      end
+    end
+  end
+end
---
+++ spec/useful_objects/string_extender_spec.rb
@@ -0,0 +1,3 @@
+RSpec.describe UsefulObjects::StringExtender do
+  using StringExtender
+end
