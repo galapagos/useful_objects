500bb913785e009240f019afbd28cf2fd80b8fd4
k-motoyan <k.motoyan888@gmail.com>
2016-03-21 10:55:33 +0900

    Appended skelton metod parse_json, parse_json! to stirng extender.

--- lib/useful_objects/string_extender.rb
+++ lib/useful_objects/string_extender.rb
@@ -4,6 +4,9 @@ module UsefulObjects
     refine String do
       def parse_json
       end
+
+      def parse_json!
+      end
     end
   end
 end
--- spec/useful_objects/string_extender_spec.rb
+++ spec/useful_objects/string_extender_spec.rb
@@ -1,3 +1,13 @@
 RSpec.describe UsefulObjects::StringExtender do
   using StringExtender
+
+  describe 'instance methods' do
+    describe 'parse_json' do
+      pending 'todo'
+    end
+
+    describe 'parse_json!' do
+      pending 'todo'
+    end
+  end
 end
