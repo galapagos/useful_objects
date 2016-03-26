e62be2e390dd8f1995ad949fbbe4cbfe119857d3
k-motoyan <k.motoyan888@gmail.com>
2016-03-26 21:48:40 +0900

    Remove String.parse_json!

--- spec/useful_objects/string_extender_spec.rb
+++ spec/useful_objects/string_extender_spec.rb
@@ -15,22 +15,5 @@ RSpec.describe UsefulObjects::StringExtender do
         it { is_expected.to be_nil }
       end
     end
-
-    describe 'parse_json!' do
-      subject { value.parse_json! }
-
-      context 'when value is json' do
-        let(:value) { '{ "foo": "foo", "bar": 1 }' }
-        it { is_expected.to include('foo' => 'foo', 'bar' => 1) }
-      end
-
-      context 'when value is not json' do
-        let(:value) { '{ "foo": foo, "bar": 1 }' }
-
-        it 'is expected to raise JSON::ParserError' do
-          expect { value.parse_json! }.to raise_error JSON::ParserError
-        end
-      end
-    end
   end
 end
